e34c203a648bc42ad67c9272161bc5e995f48464
pausone <pausone@users.noreply.github.com>
2019-02-06 09:01:41 +0100

    Add boostrap
    Add boostrap links.
    Add bootrap table.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,42 @@
 @extends('layouts.app')
 
 @section('title')
-	<h1>Welcome to Todo!</h1> 
+	<h1>{{$title}}</h1> 
 @endsection
 
 @section('content')
-	<p>This is the start page</p> 	
-	<a href="/about">About</a>
+	<a href="/about">About  </a>
 	<a href="/services">Services</a>
+	<table class="table">
+	  <thead>
+	    <tr>
+	      <th scope="col">#</th>
+	      <th scope="col">First</th>
+	      <th scope="col">Last</th>
+	      <th scope="col">Handle</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <tr>
+	      <th scope="row">1</th>
+	      <td>Mark</td>
+	      <td>Otto</td>
+	      <td>@mdo</td>
+	    </tr>
+	    <tr>
+	      <th scope="row">2</th>
+	      <td>Jacob</td>
+	      <td>Thornton</td>
+	      <td>@fat</td>
+	    </tr>
+	    <tr>
+	      <th scope="row">3</th>
+	      <td>Larry</td>
+	      <td>the Bird</td>
+	      <td>@twitter</td>
+	    </tr>
+	  </tbody>
+	</table>
 	@php
-		//Add table of TODO-items
 	@endphp
 @endsection
