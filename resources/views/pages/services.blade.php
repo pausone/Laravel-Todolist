b30455a55785b5544b9d10129352f111be61740e
pausone <pausone@users.noreply.github.com>
2019-02-06 15:31:54 +0100

    Put services in bootstrap list group

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -7,9 +7,9 @@
 @section('content')
 	<p>This is the SERVICES page</p> 
 	@if (count($services) > 0)
-		<ul>
+		<ul class="list-group">
 			@foreach ($services as $service)
-				<li>{{$service}}</li>
+				<li class="list-group-item">{{$service}}</li>
 			@endforeach
 		</ul>
 	@endif
