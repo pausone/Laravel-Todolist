bf8b814a56ebe2da398bb832bdf2c84c9d2dc0c4
pausone <pausone@users.noreply.github.com>
2019-02-07 08:56:15 +0100

    Move tasks table to seperate view

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 
-@section('title')
-	<h1>{{$title}}</h1> 
-@endsection
-
 @section('content')
+<h1>{{$title}}</h1> 
 <div class="jumbotron">
     <div class="jumbotron text-center">
         <h1>Welcome To Laravel!</h1>
@@ -13,36 +10,5 @@
 	</div>
 </div>
 
-	<table class="table">
-	  <thead>
-	    <tr>
-	      <th scope="col">#</th>
-	      <th scope="col">First</th>
-	      <th scope="col">Last</th>
-	      <th scope="col">Handle</th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	    <tr>
-	      <th scope="row">1</th>
-	      <td>Mark</td>
-	      <td>Otto</td>
-	      <td>@mdo</td>
-	    </tr>
-	    <tr>
-	      <th scope="row">2</th>
-	      <td>Jacob</td>
-	      <td>Thornton</td>
-	      <td>@fat</td>
-	    </tr>
-	    <tr>
-	      <th scope="row">3</th>
-	      <td>Larry</td>
-	      <td>the Bird</td>
-	      <td>@twitter</td>
-	    </tr>
-	  </tbody>
-	</table>
-	@php
-	@endphp
+
 @endsection
