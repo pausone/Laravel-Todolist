43aed03b964b84feaa825e5c2ef1b5485a921e11
pausone <pausone@users.noreply.github.com>
2019-02-08 09:49:52 +0100

    Add edit link

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-	<a href="/tasks" class="btn btn-outline-primary">Go back</a><br><br>
-	<h1>{{$task->description}}</h1>	
+	<a href="/tasks" class="btn btn-outline-primary">Go back</a><hr>
+	<h1>{{$task->description}}</h1>
+	<hr>	
 	<p>Due date: {{$task->due_date}}</p>
 	<p>Category id: {{$task->category_id}}</p>
 	<p>Created at: {{$task->created_at}}</p>
+	<hr>
+	<a href="/tasks/{{$task->id}}/edit" class="btn btn-outline-primary">Edit</a>
+
 @endsection
