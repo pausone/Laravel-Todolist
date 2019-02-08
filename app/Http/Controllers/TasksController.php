a9b3854fcd58404085fc25195557ece57ba3d26a
pausone <pausone@users.noreply.github.com>
2019-02-08 09:48:47 +0100

    Enable edit

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -74,7 +74,8 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-        //
+        $task = Task::find($id);
+        return view('tasks.edit')->with('task', $task);
     }
 
     /**
