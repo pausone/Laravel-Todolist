8c8f2f041596c6807f3ae012fef751f8f83647b7
pausone <pausone@users.noreply.github.com>
2019-02-11 14:16:19 +0100

    Remove category

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,19 +4,15 @@
 	<a href="/tasks" class="btn btn-outline-primary">Go back</a><br><br>
 	<h1>Edit task</h1>
 	{!! Form::open(['action' => ['TasksController@update', $task->id], 'method' => 'POST']) !!}
-	 <div class="form-group">
-	 	{{Form::label('description', 'Todo')}}
-	 	{{Form::text('description', $task->description, ['class' => 'form-control', 'placeholder' => 'Description'])}}
-	 </div>
-	 <div class="form-group">
-	 	{{Form::label('due_date', 'Due date')}}
-	 	{{Form::text('due_date', $task->due_date, ['class' => 'form-control', 'placeholder' => 'YYYYMMDD'])}}
-	 </div>
-	 <div class="form-group">
-	 	{{Form::label('category_id', 'Category')}}
-	 	{{Form::select('category_id', ['1' => 'Personal', '2' => 'Work'])}}
-	 </div>
-	 {{Form::hidden('_method', 'PUT');}}
-	{{Form::submit('Submit',['class' => 'btn btn-primary'])}}
+		<div class="form-group">
+		 	{{Form::label('description', 'Todo')}}
+		 	{{Form::text('description', $task->description, ['class' => 'form-control', 'placeholder' => 'Description'])}}
+		</div>
+		<div class="form-group">
+		 	{{Form::label('due_date', 'Due date')}}
+		 	{{Form::text('due_date', $task->due_date, ['class' => 'form-control', 'placeholder' => 'YYYYMMDD'])}}
+		</div>
+		{{Form::hidden('_method', 'PUT')}}
+		{{Form::submit('Submit',['class' => 'btn btn-primary'])}}
 	{!! Form::close() !!}	
 @endsection
