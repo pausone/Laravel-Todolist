af353631f025eebcb97d28461145f650f5fccbd0
pausone <pausone@users.noreply.github.com>
2019-02-12 10:21:31 +0100

    Add delete button and organize table

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,22 +6,24 @@
 		<p>{{count($tasks)}} items</p>
 		<table class="table">
 		    <thead>
-		        <tr>
-		            <th scope="col">#</th>
+		        <tr>		           
+		            <th scope="col">Duedate</th> 
 		            <th scope="col">Todo</th>
-		            <th scope="col">Duedate</th>
 		            <th scope="col">Actions</th>
 		        </tr>
 		  	</thead>
 			<tbody>
 				@foreach ($tasks as $task)
 					<tr>
-						<th scope="row">{{$task->id}}</th>
-						<td>{{$task->description}}</td>
 						<td>{{$task->due_date}}</td>
+						<td>{{$task->description}}</td>						
 						<td>
 							<a href="/tasks/{{$task->id}}"  class="btn btn-outline-primary">View</a>
 							<a href="/tasks/{{$task->id}}/edit" class="btn btn-outline-primary">Edit</a>
+                            {!!Form::open(['action' => ['TasksController@destroy', $task->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+                                {{Form::hidden('_method', 'DELETE')}}
+                                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                            {!!Form::close()!!}
 						</td>
 					</tr>
 				@endforeach
