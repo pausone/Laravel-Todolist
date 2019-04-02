6708e5b95ce859ae37b69db49a4c2769b460a3ca
pausone <pausone@users.noreply.github.com>
2019-04-02 11:16:11 +0200

    Clean file

--- public/js/app.js
+++ public/js/app.js
@@ -49173,171 +49173,52 @@ $(document).ready(function () {
 
   $('.datepicker').datepicker({}); //-------------------------Validation with jQuery validation plugin-----------------------------------------------
 
-  $.validator.addMethod('lettersOnly', function (value, element) {
-    return /[a-zA-ZäöüßÄÖÜ]/.test(value);
-  }, 'Endast bokstäver');
-  $.validator.addMethod('pwdCheck', function (value) {
-    return /[A-Z]+/.test(value) && /[a-z]+/.test(value) && /[\d\W]+/.test(value) && /\S{8,}/.test(value);
-  }, 'Måste innehålla minst en stor och liten bokstav samt en siffra.');
-  $.validator.addMethod('filename', function (value, element) {
-    return /^[\w,\s-]+\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP)$/.test(value);
-  }, 'Endast bokstäver');
-  $.validator.addMethod('postNumber', function (value, element) {
-    return /^\d{3} \d{2}$/.test(value);
-  }, 'Ogiltigt postnummer. Exempel: 123 45');
-  $.validator.addMethod('emailCustom', function (value, element) {
-    return /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/.test(value);
-  }, 'Ogiltig email');
   $.validator.addMethod('dateCustom', function (value, element) {
     return /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(value);
-  }, 'Datum enligt format DD/MM/YYYY');
-  $('#create-task-form').validate({
+  }, 'Date in format DD/MM/YYYY');
+  $('#todo-form').validate({
     rules: {
-      /*			
-      "description":{
-      required: true,
-      minlength: 3,
-      maxlength: 255,
-      },
-      "create_due_date":{
-      required: true,
-      dateCustom: true
-      }/*,
-      category:{
-      required: true,
-      minlength: 3,
-      maxlength: 16,
-      lettersOnly: true
-      },
-      original_price:{
-      required: true,
-      maxlength: 8,
-      digits: true
-      },
-      image_filename:{
-      required: true,
-      minlength: 3,
-      maxlength: 16,
-      filename: true
-      },
-      in_stock:{
-      required: true,
-      maxlength: 16,
-      digits: true
-      }*/
-    }
-    /*messages: {
-    	sales_price:{
-    		maxlength: "Du får max skriva 8 siffror"
-    	},
-    	original_price:{
-    		maxlength: "Du får max skriva 8 siffror"
-    	},
-    	in_stock:{
-    		maxlength: "Du får max skriva 16 siffror"
-    	}		
-    }*/
-
-  });
-  $(".product-row").each(function () {
-    $(this).validate({
-      rules: {
-        add_quantity: {
-          required: true,
-          maxlength: 16,
-          digits: true
-        }
-      },
-      messages: {
-        add_quantity: {
-          maxlength: "Du får max skriva 16 siffror"
-        }
-      }
-    });
-  });
-  $('#new-user').validate({
-    rules: {
-      username: {
+      "description": {
         required: true,
-        minlength: 3,
-        maxlength: 16
+        minlength: 1,
+        maxlength: 255
       },
-      password: {
+      "due_date": {
         required: true,
-        minlength: 8,
-        maxlength: 20,
-        pwdCheck: true
-      },
-      first_name: {
-        required: true,
-        maxlength: 30,
-        lettersOnly: true
-      },
-      last_name: {
-        required: true,
-        maxlength: 30,
-        lettersOnly: true
-      },
-      adress: {
-        required: true,
-        minlength: 3,
-        maxlength: 16
-      },
-      postal: {
-        required: true,
-        minlength: 6,
-        maxlength: 6,
-        postNumber: true
-      },
-      city: {
-        required: true,
-        maxlength: 30,
-        lettersOnly: true
+        dateCustom: true
       }
     },
     messages: {
-      password: {
-        minlength: "Du måste skriva minst 8 tecken.",
-        maxlength: "Du får skriva max 20 tecken."
-      },
-      postal: {
-        minlength: "Minst 6 tecken",
-        maxlength: "Max 6 tecken"
-      },
-      first_name: {
-        maxlength: "Max 30 bokstäver"
-      },
-      last_name: {
-        maxlength: "Max 30 bokstäver"
-      },
-      city: {
-        maxlength: "Max 30 bokstäver"
+      description: {
+        minlength: "Atleast 1 character",
+        maxlength: "Maximum 255 characters"
       }
     }
   });
-  $('#login-form').validate({
-    rules: {
-      username: {
-        required: true
-      },
-      password: {
-        required: true
-      }
-    }
-  });
-  $("#subscribe-form").validate({
-    rules: {
-      email: {
-        required: true,
-        emailCustom: true
-      }
-    }
-  });
+  /*$('#todo-form').each(function() {
+      $(this).validate({     
+  rules: {			
+  "description":{
+  	required: true,
+  	minlength: 1,
+  	maxlength: 255,
+  },
+  "due_date":{
+  	required: true,
+  	dateCustom: true
+  }
+  },
+  messages: {
+  description:{
+  	minlength: "Atleast 1 character",
+  	maxlength: "Maximum 255 characters"
+  }	
+  }
+      });
+  });*/
+
   jQuery.extend(jQuery.validator.messages, {
-    required: 'Du måste fylla i detta fält.',
-    lettersOnly: 'Du får endast använda bokstäver',
-    digits: 'Du får endast skriva ett heltal',
-    filename: 'Giltiga filformat: png,jpg,gif,bmp,jpeg. Ej å, ä, ö i filnamnet.'
+    required: 'You have to fill in this field'
   });
 });
 
